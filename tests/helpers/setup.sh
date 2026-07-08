8b6e5d826dbaf9796044496c25cf8421218fc72d
Matt Hawley <r.matt.hawley@gmail.com>
2026-07-08 15:19:34 -0500

    Abstract test file creation

---
+++ tests/helpers/setup.sh
@@ -0,0 +1,17 @@
+begin("$@") {
+    temp_dir=$(mktemp -d)
+}
+
+# Create Temp files
+create_test_files() {
+    
+    for file in "$@"; do
+        # Do nothing (:) and redirect output to create the file
+        : > "$temp_dir/$file"
+    done
+}
+
+# Delete the temp directory after the test
+cleanup() {
+    rm -rf "$temp_dir"
+}
