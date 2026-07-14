6260d647572dbcc5196366c057abc0cb27a7b1bb
Matt Hawley <r.matt.hawley@gmail.com>
2026-07-13 19:41:37 -0500

    Describe regex operations

--- lib/convert.sh
+++ lib/convert.sh
@@ -2,6 +2,10 @@
 convert_filename() {
     local name="$1"
 
+    # Convert PascalCase to snake_case.
+    # sed: Insert underscore before uppercase letters that are
+    #      preceded by lowercase letters or digits.
+    # tr: Translate all uppercase letters to lowercase.
     echo "$name" |
         sed -E 's/([a-z0-9])([A-Z])/\1_\L\2/g' |
         tr '[:upper:]' '[:lower:]'
